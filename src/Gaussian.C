123f48797a08faedf4c50bee8845aa4591f55ccc
oxon <oxon@mac.com>
2016-04-27 03:18:23 +0900

    Add Gaussian.{C,pdf} and StandardError.{C,pdf}

---
+++ src/Gaussian.C
@@ -0,0 +1,28 @@
+void Gaussian() {
+  const Int_t kN = 4;
+  const Double_t kSigma[kN] = {1., 3., .5, 2.};
+  const Double_t kMean[kN] = {0., 0., 1., -2.};
+  TF1* f[kN];
+  TLegend* leg = new TLegend(0.15, 0.6, 0.4, 0.85);
+
+  for(Int_t i = 0; i < kN; i++) {
+    f[i] = new TF1(Form("Gauss%d", i), "1/(2*pi*[0]**2)**0.5*exp(-(x-[1])**2/(2*[0]**2))", -10., 10.);
+    f[i]->SetParameter(0, kSigma[i]);
+    f[i]->SetParameter(1, kMean[i]);
+    f[i]->SetLineColor(i + 1);
+    f[i]->SetLineStyle(i + 1);
+    f[i]->SetNpx(1000);
+    f[i]->Draw(i == 0 ? "" : "same");
+
+    if (i == 0) {
+      f[i]->GetXaxis()->SetTitle("#it{x}");
+      f[i]->GetYaxis()->SetTitle("#it{f}(#it{x})");
+      f[i]->SetTitle("");
+      f[i]->GetYaxis()->SetRangeUser(0, 1);
+    }
+
+    leg->AddEntry(f[i], Form("#it{#mu} = %4.1f, #it{#sigma} = %.1f", kMean[i], kSigma[i]), "l");
+  }
+
+  leg->Draw();
+}
---
+++ src/StandardError.C
@@ -0,0 +1,26 @@
+void StandardError() {
+  const Int_t kSampleSize = 10000;
+  const Int_t kRepeat = 10000;
+  const Double_t kMean = 0.;
+  const Double_t kSigma = 1.;
+
+  TH1D* hMeanError = new TH1D("hMeanError", ";<#it{x}>", 100, -0.05, 0.05);
+  TH1D* hStdDevError = new TH1D("hStdDevError", ";#it{#sigma}_{#it{x}}", 100, -0.05, 0.05);
+
+  for(Int_t i = 0; i < kRepeat; i++){
+    TH1D h("", "", 100, -5, 5);
+    for(Int_t j = 0; j < kSampleSize; j++){
+      Double_t x = gRandom->Gaus(kMean, kSigma);
+      h.Fill(x);
+    }
+    hMeanError->Fill(h.GetMean() - kMean);
+    hStdDevError->Fill(h.GetStdDev() - kSigma);
+  }
+
+  TCanvas* can = new TCanvas("can", "can", 1200, 600);
+  can->Divide(2, 1, 1e-10, 1e-10);
+  can->cd(1);
+  hMeanError->Draw();
+  can->cd(2);
+  hStdDevError->Draw();
+}
