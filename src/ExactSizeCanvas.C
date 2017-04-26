1124c7a10e8e6177a4a1a7c0e4fc1842f8326203
oxon <oxon@mac.com>
2017-04-26 10:37:09 +0900

    Rename function PreciseSizeCanvas to ExactSizeCanvas

--- src/ExactSizeCanvas.C
+++ src/ExactSizeCanvas.C
@@ -1,5 +1,5 @@
-TCanvas* PreciseSizeCanvas(const char* name, const char* title, Double_t width,
-                           Double_t height) {
+TCanvas* ExactSizeCanvas(const char* name, const char* title, Double_t width,
+                         Double_t height) {
   TCanvas* can = new TCanvas(name, title, width, height);
   can->SetWindowSize(width + (width - can->GetWw()),
                      height + (height - can->GetWh()));
