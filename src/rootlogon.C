7234f6b4ea3ad0a8c84bb307e6b90fc43147f0ff
oxon <oxon@mac.com>
2018-04-13 11:42:57 +0900

    Update the default settings in rootlogon.C for better display as of 2018

--- src/rootlogon.C
+++ src/rootlogon.C
@@ -1,11 +1,18 @@
 {
-  // You MUST do the following settings regardless of your taste
-  gROOT->SetStyle("Plain");      // Set the overall design to "Plain" mode
-  gStyle->SetTitleBorderSize(0); // Remove the boarder from title panels
-  gStyle->SetFrameFillColor(0);  // Set the frame color to white
-  gStyle->SetCanvasColor(0);     // Set the canvas color to white
-  gStyle->SetPadBorderSize(0);   // Remove uneccesary margin in pads
-  gStyle->SetPalette(1);         // Rainbow color palette
+  // Recommended color settings for color vision deficiency
+  gStyle->SetPalette(kBird);   // Default in recent ROOT, but just in case.
+
+  // 0 : White, 1 : K,  2 : R,  3 : G,
+  // 4 : B,     5 : C,  6 : M,  7 : Y
+  // Avoid bright yellow and green for better presentation.
+  gROOT->GetColor(3)->SetRGB(0., .7, 0.); // Green  (0., 1., 0.)->(0.,  .7,  0.)
+  gROOT->GetColor(5)->SetRGB(1., .5, 0.); // Yellow (0., 1., 0.)->(0.,  .7,  0.)
+  gROOT->GetColor(7)->SetRGB(.2, .6, .6); // Cyan   (.4, 1., 1.)->(.25,.25, .75)
+ 
+  // Okumura's preference
+  gStyle->SetPadBorderSize(0); // Remove uneccesary margin in pads
+  gStyle->SetFuncWidth(2); // Set the default line width of functions to 2
+  gStyle->SetLegendBorderSize(0); // Remove the legend boarder
 
   // Change the default position of titles to top-center (optional)
   gStyle->SetTitleAlign(22);
@@ -17,6 +24,7 @@
   gStyle->SetPadTickY(1);
 
   // Set the default font to Times (optional)
+  // This is optional, but recommended if you mix TLatex
   Int_t fontid = 132;
   gStyle->SetStatFont(fontid);
   gStyle->SetLabelFont(fontid, "XYZ");
@@ -25,7 +33,5 @@
   gStyle->SetTitleFont(fontid, "");
   gStyle->SetTitleOffset(1.2, "XYZ");
   gStyle->SetTextFont(fontid);
-
-  gStyle->SetFuncWidth(2); // Set the default line width of functions to 2
-  gStyle->SetLegendBorderSize(0); // Remove the legend boarder
+  gStyle->SetLegendFont(fontid);
 }
